23dd503cf8766906263a7cfc5ebf207dbd2989d7
Raphael Mangubat <raphaelmangubat@gmail.com>
2019-03-15 19:44:49 -0500

    making changes

--- js/index.js
+++ js/index.js
@@ -9,3 +9,18 @@ new Chartist.Line('.ct-chart', {
     width: 500,
     height: 300
   });
+
+var request;
+  if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    request = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { // IE
+    try {
+      request = new ActiveXObject('Msxml2.XMLHTTP');
+    } 
+    catch (e) {
+      try {
+        request = new ActiveXObject('Microsoft.XMLHTTP');
+      } 
+      catch (e) {}
+    }
+  }
